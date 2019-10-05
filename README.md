9aebe95c095469b466a81d16074ccd5bdc8ca650
Carlos Rojas <c2rojas75@gmail.com>
2019-10-05 02:24:59 -0700

    Updated HTML/CSS layout. Have working password generator in console

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password-Generator
+
+## The general purpose of this code is to take in values from the user to determine what type of code they want. Whether, lower case, uppper case, special characters, numbers, etc. Then the user would verify the type of password they want and then click generate.
+
+## After clicking generate, a random number will determine each character. Then output to the text box. Then the user may click "Copy to Clipboard" to copy the new password to the clipboard.
+
+### 1st commit was to push code from laptop to github to work on desktop and start a new repo.
+
+### 2nd commit: General format of webpage is complete. Javascript file prompts user for information of the type of passwork the user would like. Password is generated. No errors. TO-DO: Make button call prompts.
---
+++ script.js
@@ -0,0 +1,116 @@
+var lengthPass;
+var lengthNumberCheck=false;
+var lowerCase=false;
+var upperCase=false;
+var numberTrue=false;
+var specialCharTrue=false; 
+var checkPasswordTypes;
+var count=0;
+var i=0;
+
+
+// window.onload = function(){
+//     lengthPass = parseInt(prompt("How long of a password would you like? (8-128)"))
+//     lengthNumberCheck = Number.isInteger(lengthPass);
+//     console.log(lengthNumberCheck);
+//     checkLength();
+// }
+
+window.addEventListener('load', function() {
+    lengthPass = parseInt(prompt("How long of a password would you like? (8-128)"))
+    lengthNumberCheck = Number.isInteger(lengthPass);
+    console.log(lengthNumberCheck);
+    checkLength();
+ },);
+
+function checkLength(){
+    if(lengthPass>=8 && lengthPass<=128 && lengthNumberCheck){
+        lengthPass = Math.round(lengthPass);
+        passwordType();
+    }
+    else{
+        while(lengthPass < 8 || lengthPass > 128 || lengthNumberCheck == false ){
+            lengthPass = parseInt(prompt("Enter a length between (8-128)"))
+            lengthNumberCheck = Number.isInteger(lengthPass);
+            console.log(lengthPass)
+            console.log(lengthNumberCheck)
+        }
+        lengthPass = Math.round(lengthPass);
+        console.log("here")
+        passwordType();
+
+    }
+}
+
+function passwordType(){
+
+    while(lowerCase == false && upperCase == false && numberTrue == false && specialCharTrue == false){
+        lowerCase = confirm("Would you like lower case?");
+        upperCase = confirm("Would you like upper case?");
+        numberTrue = confirm('How about numbers?');
+        specialCharTrue = confirm('Did you want to maybe use some special characters?');
+        if(lowerCase == false && 
+            upperCase == false &&
+            numberTrue == false &&
+            specialCharTrue == false){
+                alert("You have to choose at least one type.");
+            }
+    }
+    checkDecision();
+}
+
+function checkDecision(){
+    
+    checkPasswordTypes = confirm("It will have\nLower Case: " + lowerCase + "\nUpper Case: " + upperCase + '\nNumbers: ' +  numberTrue + "\nSpecial Characters: " + specialCharTrue);
+
+    if(checkPasswordTypes == false){
+        lowerCase = false;
+        upperCase = false;
+        numberTrue = false;
+        specialCharTrue = false;
+        passwordType()
+    }else{
+        generatePassword(lowerCase, upperCase, numberTrue, specialCharTrue, lengthPass);
+
+    }
+}
+
+
+function generatePassword(l, u, n, s, lP){
+    var lower = "abcdefghijklmnopqrstuvwxyz"
+    // var lowerChar = lower.split('');
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // var upperChar = upper.split('');
+    var numbers = "1234567890";
+    // var numbersChar = numbers.split('');
+    var special = ' !"' + "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    // var specialChar = special.split('');
+    var passString = "";
+    var passArray;
+    var password = "";
+    // console.log(lowerChar[3])
+    if(l){
+        passString = passString + lower;
+    }
+    if(u){
+        passString = passString + upper;
+    }
+
+    if(n){
+        passString = passString + numbers;
+    }
+    if(s){
+        passString = passString + special;
+    }
+    passArray = passString.split('');
+    for(i=0; i<lengthPass; i++){
+        password = password + passArray[Math.floor(Math.random() * passArray.length)];
+    }
+    
+    console.log(passString)
+    console.log(passArray[1])
+    console.log(password)
+
+}
+
+// checkLength()
